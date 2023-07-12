84e60054aa94c95e4e67d368a6cf2a42a82a9c0e
JAYDEEP MEHTA <jaydeepmehta15@gmail.com>
2023-07-12 20:08:56 +0530

    Updated driver details, instrumentation

--- src/main/java/com/perf/DBPerformanceTestUtil/App.java
+++ src/main/java/com/perf/DBPerformanceTestUtil/App.java
@@ -1,15 +1,24 @@
 package com.perf.DBPerformanceTestUtil;
 
+import java.security.SecureRandom;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 
 public class App 
 {
-    public static void main( String[] args ) throws ClassNotFoundException
+    public static void main( String[] args ) throws ClassNotFoundException, InterruptedException
     {
-    	Class.forName(null);
+    	Class.forName("com.mysql.jdbc.Driver");
+    	final Random random = new Random();
+    	List<Thread> threads = new ArrayList<Thread>();
         
         Runnable runthread = new Runnable() {
 			
@@ -19,30 +28,37 @@ public class App
 				
 				Connection jdbcConnection = null;
 				try {
-					jdbcConnection = DriverManager.getConnection(null, null, null);
+					jdbcConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "Backspace_2020");
 					for(int p=0; p < 50000; p++) {
 						
+						System.out.println("Current step" + p);
 						java.sql.PreparedStatement stmt = jdbcConnection.prepareStatement("Insert into users(username, firstname, lastname, email, department) values (?,?,?,?,?)");
-						stmt.setString(0, "UN" + p);
-						stmt.setString(1, "Ramlal" + p);
-						stmt.setString(2, "Sharma" + p);
-						stmt.setString(3, "r@testdomain.local" + p);
-						stmt.setString(4, "Sales");
+						stmt.setString(1, "UN" + random.nextInt(1000000));
+						stmt.setString(2, "Ramlal" + p);
+						stmt.setString(3, "Sharma" + p);
+						stmt.setString(4, "r@testdomain.local" + p);
+						stmt.setString(5, "Sales");
 						
-						stmt.execute();
-						System.out.println("THreadId execute sucess" + Thread.currentThread().getId());
+						try {
+							stmt.execute();
+							System.out.println("THreadId execute sucess" + Thread.currentThread().getId());
+						}catch (SQLException e) {
+							//e.printStackTrace();
+						}
+						
 				}
 				}catch (SQLException e) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}finally {
+					//e.printStackTrace();
+				}
+				finally {
 					
 					try {
 						jdbcConnection.close();
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+						//e.printStackTrace();
+					} 
 				}
 				
 				}
@@ -50,13 +66,18 @@ public class App
 			};
 		
         
-        for(int i=0;i<10;i++) {
+        for(int i=0;i<30;i++) {
         
         	Thread thr = new Thread(runthread);
         	thr.start();
-        	
+        	threads.add(thr);
         }
-        	
+        //Join to main to avoid program termination before threads finish
+        for (Thread thread : threads) {
+        	thread.join();
+			
+		}
+        
         	
         	
         	
